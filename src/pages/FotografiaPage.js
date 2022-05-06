1aaea91c49a9314f1e4264281f792cc27fe7580c
unknown <germangabriel1998@gmail.com>
2022-05-06 16:10:16 -0300

    added more images

--- src/pages/FotografiaPage.js
+++ src/pages/FotografiaPage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import Companchiro from '../imgEj.jpg';
-import AMUI from '../imgEj2.jpg';
+import imgej from '../media/imgEj.jpg';
+import imgej2 from '../media/imgEj2.jpg';
+import imgej3 from '../media/imgEj3.jpg';
+import imgej4 from '../media/imgEj4.png';
+import imgej5 from '../media/imgEj5.jpg';
+import imgej6 from '../media/imgEj6.jpg';
 import { Carousel } from 'react-bootstrap';
 import TrabajoBlock from '../components/TrabajoBlock';
 import { NavLink } from 'react-router-dom';
 
 export default function FotografiaPage() {
-const Rengoku = AMUI ;
-const Nashetampicho = Companchiro;
+const ImgEj = imgej;
+const ImgEj2 = imgej2 ;
+const ImgEj3 = imgej3 ;
+const ImgEj4 = imgej4 ;
+const ImgEj5 = imgej5 ;
+const ImgEj6 = imgej6 ;
+
   return (
     <motion.div initial={{opacity:0}}animate={{opacity:1}}exit={{opacity:0}} className="fotografiaPage">  
     
@@ -17,43 +26,43 @@ const Nashetampicho = Companchiro;
       
       <div className="trabajosWrapper">
           <NavLink to='/PortafolioNacho/TrabajoEjemplo'>  
-            <TrabajoBlock bgImg={Rengoku} title={"Boda Familia Gonzales"} description={"Boda familia"} />
+            <TrabajoBlock bgImg={ImgEj} title={"Boda Familia Gonzales"} description={"Boda familia"} />
           </NavLink>
           <NavLink to='/PortafolioNacho/TrabajoEjemplo'> 
-            <TrabajoBlock bgImg={Nashetampicho} title={"Festival de la Cerveza"} description={"Boda familia"} />
+            <TrabajoBlock bgImg={ImgEj2} title={"Festival de la Cerveza"} description={"Boda familia"} />
           </NavLink>
           <NavLink to='/PortafolioNacho/TrabajoEjemplo'> 
-            <TrabajoBlock bgImg={Rengoku} title={"Entrenando"} description={"Boda familia"} />
+            <TrabajoBlock bgImg={ImgEj3} title={"Entrenando"} description={"Boda familia"} />
           </NavLink>
           <NavLink to='/PortafolioNacho/TrabajoEjemplo'> 
-          <TrabajoBlock bgImg={Nashetampicho} title={"Pobreza en el pais"} description={"Boda familia"} />
+          <TrabajoBlock bgImg={ImgEj4} title={"Pobreza en el pais"} description={"Boda familia"} />
           </NavLink>
           <NavLink to='/PortafolioNacho/TrabajoEjemplo'> 
-            <TrabajoBlock bgImg={Nashetampicho} title={"Maldita sea no de nuevo"} description={"Boda familia"} />
+            <TrabajoBlock bgImg={ImgEj5} title={"Maldita sea no de nuevo"} description={"Boda familia"} />
           </NavLink>   
           <NavLink to='/PortafolioNacho/TrabajoEjemplo'> 
-            <TrabajoBlock bgImg={Nashetampicho} title={"Cagada a palos"} description={"Boda familia"} /> 
+            <TrabajoBlock bgImg={ImgEj6} title={"Cagada a palos"} description={"Boda familia"} /> 
           </NavLink>
       </div>
       
       <Carousel className='carouselStyle' fade>
         <Carousel.Item>
-          <img className='d-block carouselImg' src={Rengoku} alt="ej"></img>
+          <img className='d-block carouselImg' src={ImgEj6} alt="ej"></img>
         </Carousel.Item>
         <Carousel.Item>
-          <img className='d-block carouselImg' src={Nashetampicho} alt="ej"></img>
+          <img className='d-block carouselImg' src={ImgEj3} alt="ej"></img>
         </Carousel.Item>
         <Carousel.Item>
-          <img className='d-block carouselImg' src={Rengoku} alt="ej"></img>
+          <img className='d-block carouselImg' src={ImgEj5} alt="ej"></img>
         </Carousel.Item>
         <Carousel.Item>
-          <img className='d-block carouselImg' src={Nashetampicho} alt="ej"></img>
+          <img className='d-block carouselImg' src={ImgEj3} alt="ej"></img>
         </Carousel.Item>
         <Carousel.Item>
-          <img className='d-block carouselImg' src={Rengoku} alt="ej"></img>
+          <img className='d-block carouselImg' src={ImgEj2} alt="ej"></img>
         </Carousel.Item>
         <Carousel.Item>
-          <img className='d-block carouselImg' src={Nashetampicho} alt="ej"></img>
+          <img className='d-block carouselImg' src={ImgEj3} alt="ej"></img>
         </Carousel.Item>
       </Carousel>
       
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import SectionBlock from "../components/SectionBlock";
-import { NavLink } from 'react-router-dom';
 import '../index.css';
-import Companchiro from '../imgEj.jpg';
-import AMUI from '../imgEj2.jpg';
 import { motion } from 'framer-motion';
 
 export default function HomePage() {
-const Rengoku = AMUI ;
-const Nashetampicho = Companchiro;
   return (
-    <motion.div initial={{opacity:0}}animate={{opacity:1,transition:{duration:2}}}exit={{opacity:0}}>
+    <motion.div initial={{opacity:0,scale:.7}}animate={{opacity:1,scale:1,transition:{duration:1}}}exit={{opacity:0}}>
 
         <div className="introductionWrapper">
             <span className="titleHome">Nacho Garcia</span><br></br>
             <span className="subtitleHome">Fotografia</span>
-        </div>
-        
-        <div className="sectionsWrapper">
-            <NavLink to='/PortafolioNacho/Fotografia'>
-                <SectionBlock bgImg={Nashetampicho} title="Fotografia" description="XD"/>
-            </NavLink>
-            <NavLink to='/PortafolioNacho/Filmmaking'>
-                <SectionBlock bgImg={Rengoku} title="Filmmaking" description="XD"/> 
-            </NavLink>
-            
         </div>
 
     </motion.div>
