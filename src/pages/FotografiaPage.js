d2d9178de7656421c4e164da23223898c8929194
unknown <germangabriel1998@gmail.com>
2022-05-18 15:56:12 -0300

    Added grid to loading animation

--- src/pages/FotografiaPage.js
+++ src/pages/FotografiaPage.js
@@ -19,30 +19,33 @@ const ImgEj6 = imgej6 ;
 const variants={
   hidden:{opacity:0.5,scale:.2},
   hidden2:{scale:.7},
-  visible:{opacity:1,scale:1,transition:{duration:1}}
+  visible:{opacity:1,scale:1,transition:{duration:1}},
+  loading:{opcaciy:1,scale:1, transition:{duration:2.5}}
 }
 const [show,setShow]= useState(true);
   useEffect(()=>{
-    const timer= setTimeout(()=>{setShow(false);},1500);
+    const timer= setTimeout(()=>{setShow(false);},1111110);
     return()=> clearTimeout(timer);
   },[]);
 
   return (   
     <div>  
       <div className={show ? 'loading' : 'finishedLoading' }>
-        Cargando
-          <motion.div initial={"hidden"} animate={"visible"} variants={variants}>
+        <span className='loadingText'>Cargando</span>
+        <div className='loadingPropsWrapper'>
+          <motion.div className='loadingProp'  initial={"hidden"} animate={"loading"} variants={variants}>
             <TrabajoBlock  bgImg={ImgEj} title={"Cargando..."} description={"Loading prop"}/>
           </motion.div>
-          <motion.div initial={"hidden"} animate={"visible"} variants={variants}>
+          <motion.div className='loadingProp' initial={"hidden"} animate={"loading"} variants={variants}>
             <TrabajoBlock  bgImg={ImgEj2} title={"Cargando..."} description={"Loading prop"}/>
           </motion.div>
-          <motion.div initial={"hidden"} animate={"visible"} variants={variants}>
+          <motion.div className='loadingProp' initial={"hidden"} animate={"loading"} variants={variants}>
             <TrabajoBlock  bgImg={ImgEj3} title={"Cargando..."} description={"Loading prop"}/>
           </motion.div>
-          <motion.div initial={"hidden"} animate={"visible"} variants={variants}>
+          <motion.div className='loadingProp' initial={"hidden"} animate={"loading"} variants={variants}>
             <TrabajoBlock  bgImg={ImgEj4} title={"Cargando..."} description={"Loading prop"}/>
           </motion.div>
+        </div>
       </div>
         
         
